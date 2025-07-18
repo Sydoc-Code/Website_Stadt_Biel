b757672f1c55aa9c7a706906b9c8573ba03a7dbb
Sydoc-Code <benjamin.streich@sydoc.ch>
2025-07-18 13:54:44 +0200

    host and port updated

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ def update_status():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
+    # app.run(debug=True)
